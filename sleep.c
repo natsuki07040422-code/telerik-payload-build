5c8f99cbb3d6b796bc384d149550f2f298fb48a2
natsuki <natsuki@synapse.local>
2026-06-29 09:18:31 -0300

    Add build workflow for SleepInstaller mixed-mode DLLs

---
+++ sleep.c
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
+{
+    if (fdwReason == DLL_PROCESS_ATTACH)
+        Sleep(10000);
+    return TRUE;
+}
